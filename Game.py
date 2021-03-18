82c5307343ec7b509abb987c276dfb5bc9044a9a
elPytel <jaroslav.korner1@gmail.com>
2021-03-18 22:08:30 +0100

    split Game to score+game

--- Game.py
+++ Game.py
@@ -2,67 +2,12 @@
 """
 Funkce pro hru 2048.
 """
+import Score
 import random
 import os
 
 MOVE = ["w","a","s","d"]
 DEBUG = True
-
-class Score:
-	
-	def __init__ (self, size, name, score):
-		self.size = size
-		self.name = name
-		self.score = score
-		
-	def Print (self):
-		print("Size:", self.size, "\tName:", self.name, "\tscore:", self.score)
-
-class ScoreBoard:
-		
-	def __init__ (self):
-		self.data = []
-		
-	def add (self, size, name, score):
-		score = Score(size, name, score)
-		self.data.append(score)
-		# tohle je sice pomaly, ale co se da delat
-		"""ut.sort(key=lambda x: x.count, reverse=True)"""
-		self.data.sort(key=lambda x: x.score)
-		
-	def parse (self, string):
-		array = string.split()
-		size = int(array[1])
-		name = array[3]
-		score = int(array[5])
-		return [size, name, score]
-		
-	def Import (self, file):
-		# existuje?
-		file_exists = os.path.isfile(file)
-		if file_exists:		# vytvori pokud neexistuje
-			file = open(file, 'r', encoding='utf-8')
-			for line in file.readlines():
-				array = self.parse(line)
-				score = Score(array[0], array[1], array[2])
-				self.data.append(score)
-			file.close()
-		
-	def Export (self, file):
-		file = open(file, 'w', encoding='utf-8')
-		# TODO tohle je osklivy
-		for score in self.data:
-			file.write("Size: " + str(score.size) + " Name: " + str(score.name) + " Score: " + str(score.score) + " \n")
-		
-		file.close()
-	
-	def Print (self, number):
-		if number > len(self.data):
-			number = len(self.data)
-			
-		for i in range(number):
-			score = self.data[i]
-			score.Print()
 
 class Game:
 	
@@ -74,7 +19,7 @@ class Game:
 		self.end = False
 		
 		self.logo = "logo.txt"
-		self.file_score = "score.txt"
+		self.file_score = ".score.txt"
 		self.scoreBoard = None
 		
 	def ImportScore (self):
---
+++ Score.py
@@ -0,0 +1,62 @@
+# By Pytel
+
+class Score:
+	
+	def __init__ (self, size, name, score):
+		self.size = size
+		self.name = name
+		self.score = score
+		
+	def Print (self):
+		print("Size:", self.size, "\tName:", self.name, "\tscore:", self.score)
+
+class ScoreBoard:
+		
+	def __init__ (self):
+		self.data = []
+		
+	def add (self, size, name, score):
+		score = Score(size, name, score)
+		self.data.append(score)
+		# tohle je sice pomaly, ale co se da delat
+		"""ut.sort(key=lambda x: x.count, reverse=True)"""
+		self.data.sort(key=lambda x: x.score)
+		
+	def parse (self, string):
+		array = string.split()
+		size = int(array[1])
+		name = array[3]
+		score = int(array[5])
+		return [size, name, score]
+		
+	def Import (self, file):
+		# existuje?
+		file_exists = os.path.isfile(file)
+		if file_exists:		# vytvori pokud neexistuje
+			file = open(file, 'r', encoding='utf-8')
+			for line in file.readlines():
+				array = self.parse(line)
+				score = Score(array[0], array[1], array[2])
+				self.data.append(score)
+			file.close()
+		
+	def Export (self, file):
+		file = open(file, 'w', encoding='utf-8')
+		# TODO tohle je osklivy
+		for score in self.data:
+			file.write("Size: " + str(score.size) + " Name: " + str(score.name) + " Score: " + str(score.score) + " \n")
+		
+		file.close()
+	
+	def Print (self, number):
+		if number > len(self.data):
+			number = len(self.data)
+			
+		for i in range(number):
+			score = self.data[i]
+			score.Print()
+		
+	
+"""
+END
+"""
